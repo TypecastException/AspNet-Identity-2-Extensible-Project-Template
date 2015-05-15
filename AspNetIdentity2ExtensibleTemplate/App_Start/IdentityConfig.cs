89e5198fdc1835ec4577a96004e8c435d3e81157
samlbest <sambest86@gmail.com>
2015-05-15 14:35:25 -0500

    Use DropCreateDatabaseIfModelChanges for DbInitializer

--- AspNetIdentity2ExtensibleTemplate/App_Start/IdentityConfig.cs
+++ AspNetIdentity2ExtensibleTemplate/App_Start/IdentityConfig.cs
@@ -104,7 +104,7 @@ namespace IdentitySample.Models
     // This is useful if you do not want to tear down the database each time you run the application.
     // public class ApplicationDbInitializer : DropCreateDatabaseAlways<ApplicationDbContext>
     // This example shows you how to create a new database if the Model changes
-    public class ApplicationDbInitializer : DropCreateDatabaseAlways<ApplicationDbContext> 
+    public class ApplicationDbInitializer : DropCreateDatabaseIfModelChanges<ApplicationDbContext> 
     {
         protected override void Seed(ApplicationDbContext context) {
             InitializeIdentityForEF(context);
